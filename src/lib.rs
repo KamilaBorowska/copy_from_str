7584aa1cd0abbefbc696d1d15a3d8755505e2ff2
Konrad Borowski <konrad@borowski.pw>
2020-09-16 11:45:48 +0200

    Update extension_trait crate

--- src/lib.rs
+++ src/lib.rs
@@ -28,71 +28,72 @@
 //! [`copy_from_slice`]: https://doc.rust-lang.org/std/primitive.slice.html#method.copy_from_slice
 
 #![deny(missing_docs)]
-#![recursion_limit = "256"]
 #![no_std]
-#[macro_use]
+
 extern crate extension_trait;
 
-extension_trait! {
-    /// Extension method for copying a string into another string.
-    pub CopyFromStrExt for str {
-        /// Copies all elements from `src` into `self`, using a memcpy.
-        ///
-        /// The length of `src` must be the same as `self`.
-        ///
-        /// # Panics
-        ///
-        /// This function will panic if the two strings have different lengths.
-        ///
-        /// # Examples
-        ///
-        /// Copying two elements from a string into another:
-        ///
-        /// ```
-        /// use copy_from_str::CopyFromStrExt;
-        ///
-        /// let src = "abcd";
-        /// let mut dst = String::from("  ");
-        ///
-        /// dst.copy_from_str(&src[2..]);
-        ///
-        /// assert_eq!(src, "abcd");
-        /// assert_eq!(dst, "cd");
-        /// ```
-        ///
-        /// Rust enforces that there can only be one mutable reference with no
-        /// immutable references to a particular piece of data in a particular
-        /// scope. Because of this, attempting to use `copy_from_str` on a
-        /// single string will result in a compile failure:
-        ///
-        /// ```compile_fail
-        /// use copy_from_str::CopyFromStrExt;
-        ///
-        /// let mut string = String::from("abcde");
-        ///
-        /// string[..2].copy_from_str(&string[3..]); // compile fail!
-        /// ```
-        ///
-        /// To work around this, we can use [`split_at_mut`] to create two distinct
-        /// sub-strings from a string:
-        ///
-        /// ```
-        /// use copy_from_str::CopyFromStrExt;
-        ///
-        /// let mut string = String::from("abcde");
-        ///
-        /// {
-        ///     let (left, right) = string.split_at_mut(2);
-        ///     left.copy_from_str(&right[1..]);
-        /// }
-        ///
-        /// assert_eq!(string, "decde");
-        /// ```
-        ///
-        /// [`split_at_mut`]: https://doc.rust-lang.org/std/primitive.str.html#method.split_at_mut
-        #[inline]
-        fn copy_from_str(&mut self, src: &str) {
-            unsafe { self.as_bytes_mut() }.copy_from_slice(src.as_bytes());
-        }
+use extension_trait::extension_trait;
+
+/// Extension method for copying a string into another string.
+#[extension_trait(pub)]
+impl CopyFromStrExt for str {
+    /// Copies all elements from `src` into `self`, using a memcpy.
+    ///
+    /// The length of `src` must be the same as `self`.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the two strings have different lengths.
+    ///
+    /// # Examples
+    ///
+    /// Copying two elements from a string into another:
+    ///
+    /// ```
+    /// use copy_from_str::CopyFromStrExt;
+    ///
+    /// let src = "abcd";
+    /// let mut dst = String::from("  ");
+    ///
+    /// dst.copy_from_str(&src[2..]);
+    ///
+    /// assert_eq!(src, "abcd");
+    /// assert_eq!(dst, "cd");
+    /// ```
+    ///
+    /// Rust enforces that there can only be one mutable reference with no
+    /// immutable references to a particular piece of data in a particular
+    /// scope. Because of this, attempting to use `copy_from_str` on a
+    /// single string will result in a compile failure:
+    ///
+    /// ```compile_fail
+    /// use copy_from_str::CopyFromStrExt;
+    ///
+    /// let mut string = String::from("abcde");
+    ///
+    /// string[..2].copy_from_str(&string[3..]); // compile fail!
+    /// ```
+    ///
+    /// To work around this, we can use [`split_at_mut`] to create two distinct
+    /// sub-strings from a string:
+    ///
+    /// ```
+    /// use copy_from_str::CopyFromStrExt;
+    ///
+    /// let mut string = String::from("abcde");
+    ///
+    /// {
+    ///     let (left, right) = string.split_at_mut(2);
+    ///     left.copy_from_str(&right[1..]);
+    /// }
+    ///
+    /// assert_eq!(string, "decde");
+    /// ```
+    ///
+    /// [`split_at_mut`]: https://doc.rust-lang.org/std/primitive.str.html#method.split_at_mut
+    #[allow(unused_attributes)]
+    #[inline]
+    fn copy_from_str(&mut self, src: &str) {
+        unsafe { self.as_bytes_mut() }.copy_from_slice(src.as_bytes());
     }
 }
